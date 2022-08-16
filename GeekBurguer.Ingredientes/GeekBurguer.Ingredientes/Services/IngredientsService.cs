c034928c325dc4c1b7e56e6d17fe08767d819448
EdenilsonBila <edn_santos@hotmail.com>
2022-08-16 03:14:55 -0300

    Melhoria Metodos

--- GeekBurguer.Ingredientes/GeekBurguer.Ingredientes/Services/IngredientsService.cs
+++ GeekBurguer.Ingredientes/GeekBurguer.Ingredientes/Services/IngredientsService.cs
@@ -19,17 +19,27 @@ namespace GeekBurguer.Ingredientes.Services
         public async Task<List<IngredientsResponse>> GetProductsByRestrictions(IngredientsRequest request)
         {
             List<IngredientsResponse> prodIngredients = new();
+
             var products = _productRepository.GetAll();
 
             foreach (var product in products)
             {
                 var prodHasRestriction = true;
-                foreach (var item in product.Items)
+
+                if (product.Items.All(e => e.Ingredients == null))
                 {
-                    //valida se os igredientes possui alguma das restriçoes enviadas
-                    if (item.Ingredients != null && !item.Ingredients.Any(e => request.Restrictions.Contains(e)))
+                    prodHasRestriction = false;
+                }
+                else
+                {
+                    foreach (var item in product.Items)
                     {
-                        prodHasRestriction = false;
+                        //valida se os igredientes possui alguma das restriçoes enviadas
+                        if (item.Ingredients != null && !item.Ingredients.Any(e => request.Restrictions.Contains(e)))
+                        {
+                            prodHasRestriction = false;
+                            break;
+                        }
                     }
                 }
 
@@ -50,38 +60,50 @@ namespace GeekBurguer.Ingredientes.Services
 
         public async Task GetProducts()
         {
-                 //MOCK
-                //_productRepository.Add(new Model.Products() { StoreName = "paulista", ProductId = new Guid(), Items = new List<ItemIngredients> { new ItemIngredients() { ItemId = new Guid(), Name = "meat" } } });
+            //MOCK
+            _productRepository.Add(new Model.Products()
+            {
+                StoreName = "paulista",
+                ProductId = new Guid(),
+                Items = new List<ItemIngredients> {
+                new ItemIngredients() { ItemId = new Guid(), Name = "meat" },
+                new ItemIngredients() { ItemId = new Guid(), Name = "bread" },
+                new ItemIngredients() { ItemId = new Guid(), Name = "lettuce" },
+            }
+            });
+            _productRepository.Add(new Model.Products()
+            {
+                StoreName = "paulista",
+                ProductId = new Guid(),
+                Items = new List<ItemIngredients> {
+                new ItemIngredients() { ItemId = new Guid(), Name = "cheese" },
+                new ItemIngredients() { ItemId = new Guid(), Name = "tomato" },
+                new ItemIngredients() { ItemId = new Guid(), Name = "bacon" },
+            }
+            });
 
-                
-                var products = await _productRepository.GetByStoreName("paulista");
 
-                var productsToGet = _mapper.Map<IEnumerable<Model.Products>>(products);
+            var products = await _productRepository.GetByStoreName("paulista");
 
-                if (productsToGet.Any())
-                {
-                    _productRepository.AddRange(productsToGet);
-                }
+            var productsToGet = _mapper.Map<IEnumerable<Model.Products>>(products);
+
+            if (productsToGet.Any())
+            {
+                _productRepository.AddRange(productsToGet);
+            }
         }
 
         public void MargeProductsAndIngredients(LabelImageDTO labelImageDto)
         {
             //procura os produtos que possui este item: Carne
-            var produtos = _productRepository.GetAll();
+            var produtos = _productRepository.GetAll().Where(e => e.Items.Any(x => x.Name == labelImageDto.ItemName)).ToList();
 
-            //var prods2 = _productRepository.GetAll().Where(e => e.Items.Any(x => x.Name == labelImageDto.ItemName)).ToList();
-
-            //varre os produtos procurando qual possui o item acima: CARNE
             foreach (var produto in produtos)
             {
-                //Caso o produto possua itens do enviado
-                if (produto.Items.Any(e => e.Name == labelImageDto.ItemName))
+                //Atualiza os itens com os ingredients enviados
+                foreach (var item in produto.Items.Where(e => e.Name == labelImageDto.ItemName))
                 {
-                    //Atualiza os itens com os ingredients enviados
-                    foreach (var item in produto.Items)
-                    {
-                        item.Ingredients = labelImageDto.Ingredients;
-                    }
+                    item.Ingredients = labelImageDto.Ingredients;
                 }
             }
         }
